a0222b8b9258dbc2bcbb383fdd2a1a34f943703d
Arnold Gandarillas Castillo <arkgast@gmail.com>
2026-02-09 11:21:22 -0400

    refactor(guessing_game): add loop for multiple guesses

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -3,28 +3,31 @@ use std::cmp::Ordering;
 use std::io;
 
 fn main() {
-    let mut guess = String::new();
-
     let secret_number: u8 = random_range(0..=100);
 
     println!("Secret numbert is: {}", secret_number);
 
-    println!("Insert your guess number: ");
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
+    loop {
+        let mut guess = String::new();
 
-    let guess: u8 = guess.trim().parse().expect("Failed to parse guess number");
+        println!("Insert your guess number: ");
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
 
-    match guess.cmp(&secret_number) {
-        Ordering::Less => {
-            println!("Your guess number is lower than the expected number");
-        }
-        Ordering::Equal => {
-            println!("You've guessed the secret number");
-        }
-        Ordering::Greater => {
-            println!("Your guess number is higher than the expected number");
+        let guess: u8 = guess.trim().parse().expect("Failed to parse guess number");
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => {
+                println!("Your guess number is lower than the expected number");
+            }
+            Ordering::Equal => {
+                println!("You've guessed the secret number");
+                break;
+            }
+            Ordering::Greater => {
+                println!("Your guess number is higher than the expected number");
+            }
         }
     }
 }
