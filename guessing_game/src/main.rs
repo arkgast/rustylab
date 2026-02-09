1f668671262395d9673718e5382d93dffc0ab9ca
Arnold Gandarillas Castillo <arkgast@gmail.com>
2026-02-09 10:18:03 -0400

    feat: add simple version of guessing number game

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,3 +1,30 @@
+use rand::random_range;
+use std::cmp::Ordering;
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    let mut guess = String::new();
+
+    let secret_number: u8 = random_range(0..=100);
+
+    println!("Secret numbert is: {}", secret_number);
+
+    println!("Insert your guess number: ");
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line");
+
+    let guess: u8 = guess.trim().parse().expect("Failed to parse guess number");
+
+    match guess.cmp(&secret_number) {
+        Ordering::Less => {
+            println!("Your guess number is lower than the expected number");
+        }
+        Ordering::Equal => {
+            println!("You've guessed the secret number");
+        }
+        Ordering::Greater => {
+            println!("Your guess number is higher than the expected number");
+        }
+    }
 }
