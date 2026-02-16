aa459ab4b4b9f269b15ad8b07a018693590fb512
Arnold Gandarillas Castillo <arkgast@gmail.com>
2026-02-15 20:09:35 -0400

    feat(state_machine): add Runtime to integrate balances and system pallets

--- state_machine/src/main.rs
+++ state_machine/src/main.rs
@@ -1,6 +1,41 @@
 mod balances;
 mod system;
 
+pub struct Runtime {
+    system: system::Pallet,
+    balances: balances::Pallet,
+}
+
+impl Runtime {
+    pub fn new() -> Self {
+        Self {
+            system: system::Pallet::new(),
+            balances: balances::Pallet::new(),
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut runtime = Runtime::new();
+    runtime.balances.set_balance("alice", 100);
+
+    // simulate block
+    runtime.system.inc_block_number();
+    assert_eq!(runtime.system.block_number(), 1);
+
+    // first transaction
+    runtime.system.inc_nonce("alice");
+    let transfer_result = runtime
+        .balances
+        .transfer("alice", "bob", 30)
+        .map_err(|e| eprintln!("1st tx error: {:?}", e));
+    println!("1st tx: {:?}", transfer_result);
+
+    // second transaction
+    runtime.system.inc_nonce("alice");
+    let transfer_result = runtime
+        .balances
+        .transfer("alice", "bob", 80)
+        .map_err(|e| eprintln!("2nd tx error: {:?}", e));
+    println!("2nd tx: {:?}", transfer_result);
 }
