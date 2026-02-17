d25d6229eaeef3dcd335d7d19a8f8113b424964e
Arnold Gandarillas Castillo <arkgast@gmail.com>
2026-02-17 19:16:21 -0400

    feat(calculator): add interactive input for numbers

--- calculator/src/main.rs
+++ calculator/src/main.rs
@@ -1,10 +1,36 @@
+use std::io::{Write, stdin, stdout};
+
 use calculator::Calculator;
 
 fn main() {
-    let a = 400u32;
-    let b = 200u32;
+    loop {
+        let a = read_line();
+        let a = match parse(a) {
+            Ok(n) => n,
+            Err(_) => continue,
+        };
 
-    let calculator = Calculator::new(a, b);
+        let b = read_line();
+        let b = match parse(b) {
+            Ok(n) => n,
+            Err(_) => continue,
+        };
 
-    println!("{}", calculator);
+        let calculator = Calculator::new(a, b);
+
+        println!("\n{}", calculator);
+    }
+}
+
+fn read_line() -> String {
+    let mut line = String::new();
+    print!("Insert your number: ");
+    stdout().flush().expect("Failed to flush");
+    stdin().read_line(&mut line).expect("Failed to read line");
+    line
+}
+
+fn parse(line: String) -> Result<i32, &'static str> {
+    let num: i32 = line.trim().parse().map_err(|_| "Enter a valid number.")?;
+    Ok(num)
 }
