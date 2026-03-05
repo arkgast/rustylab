01139788242293d91f0ec9551d9508d55618f653
Arnold Gandarillas Castillo <arkgast@gmail.com>
2026-03-05 19:54:27 -0400

    feat(state_machine): store inner errors inside RuntimeError variants

--- state_machine/src/errors.rs
+++ state_machine/src/errors.rs
@@ -1,6 +1,6 @@
 use std::fmt::Display;
 
-#[derive(Debug, PartialEq)]
+#[derive(Debug, PartialEq, Eq)]
 pub enum ProofOfExistenceError {
     ClaimAlreadyExists,
     ClaimNotFound,
@@ -22,6 +22,37 @@ pub enum TransferError {
 }
 
 impl Display for TransferError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{:?}", self)
+    }
+}
+
+#[derive(Debug, PartialEq, Eq)]
+pub enum RuntimeError {
+    ProofOfExistence(ProofOfExistenceError),
+    System(SystemError),
+    Transfer(TransferError),
+}
+
+impl From<ProofOfExistenceError> for RuntimeError {
+    fn from(value: ProofOfExistenceError) -> Self {
+        RuntimeError::ProofOfExistence(value)
+    }
+}
+
+impl From<SystemError> for RuntimeError {
+    fn from(value: SystemError) -> Self {
+        RuntimeError::System(value)
+    }
+}
+
+impl From<TransferError> for RuntimeError {
+    fn from(value: TransferError) -> Self {
+        RuntimeError::Transfer(value)
+    }
+}
+
+impl Display for RuntimeError {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         write!(f, "{:?}", self)
     }
